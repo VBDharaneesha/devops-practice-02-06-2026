af9e357fea97d1c32e73ff68f653a737f8f3883c
vbdharaneesha <vdharaneesha@gmail.com>
2026-06-07 17:37:26 +0000

    demo

---
+++ jenkins-installscript.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+    
+  sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+    https://pkg.jenkins.io/debian/jenkins.io-2026.key
+ echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+    https://pkg.jenkins.io/debian binary/ | sudo tee \
+    /etc/apt/sources.list.d/jenkins.list > /dev/null
+  sudo apt-get update
+  sudo apt-get install fontconfig openjdk-21-jre
+  sudo apt-get install jenkins
